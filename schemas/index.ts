1a33cdb2b5aaf8868447e2ac2af20b891fb5548d
samanzen <kh.sa62@gmail.com>
2025-07-25 17:31:42 -0700

    Create index.ts

---
+++ schemas/index.ts
@@ -0,0 +1,6 @@
+import service from './service'
+import galleryItem from './galleryItem'
+import homepage from './homepage'
+import post from './blogPost'
+
+export const schemaTypes = [service, galleryItem, homepage, post]
