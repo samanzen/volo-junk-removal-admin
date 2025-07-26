ca830e68b07263797b1ef43a680010f269ff1ecd
samanzen <kh.sa62@gmail.com>
2025-07-25 17:29:51 -0700

    Create blogPost.ts

---
+++ schemas/blogPost.ts
@@ -0,0 +1,60 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Blog Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'array', // Using 'array' for rich text (block content)
+      of: [
+        {
+          type: 'block',
+        },
+        {
+          type: 'image',
+        },
+      ],
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      media: 'mainImage',
+    },
+  },
+})
